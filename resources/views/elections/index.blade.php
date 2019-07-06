85b9bf3ae8be484ea65771a59e28ebed7bcd79b2
Katon Surya Atmaja <katonsuryaa@gmail.com>
2019-07-06 18:14:01 +0700

    Re working again

--- resources/views/elections/index.blade.php
+++ resources/views/elections/index.blade.php
@@ -15,13 +15,8 @@
             <div class="col">
                 <form>
                     <div class="input-group">
-<<<<<<< HEAD
-                      <div class="input-group-prepend">
-                        <span class="input-group-text bg-transparent shadow-sm">
-=======
                       <div class="input-group-append">
                         <span class="input-group-text bg-primary text-light border-0 shadow-sm">
->>>>>>> 15767d2... validasi fitur voters
                             <i class="fas fa-search"></i>
                         </span>
                       </div>
@@ -66,14 +61,6 @@
                                   <span class="text-secondary">
                                       <i class="fas fa-person-booth"></i> 1200 pemilih
                                   </span>
-                              </div>
-                              <div class="d-flex justify-content-end">
-                                  <a class="btn btn-info text-white btn-sm mr-1" href="{{route('elections.edit',$election->id)}}"><i class="fas fa-edit"></i> Edit</a>
-                                  <form action="{{route('elections.destroy',$election->id)}}" method="post">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button class="btn btn-danger btn-sm"><i class="fas fa-trash"></i> Hapus</button>
-                                  </form>
                               </div>
                             </div>
                           </div>
