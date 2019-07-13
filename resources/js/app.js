473191bc7dada63c663d7bf030f73fb6a5f28fb4
Katon Surya Atmaja <katonsuryaa@gmail.com>
2019-07-13 15:05:46 +0700

    Adding parsleyjs

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,6 +7,7 @@
 require('./bootstrap');
 require('./create-election-steps');
 require('select2');
+require('parsleyjs');
 
 $('.select2').select2({
     theme: 'bootstrap4',
@@ -40,4 +41,20 @@ Vue.component('datetimepicker', require('./components/DateTimePickerComponent.vu
 
 const app = new Vue({
     el: '#app',
+});
+
+window.Parsley.addValidator('maxFileSize', {
+  validateString: function(_value, maxSize, parsleyInstance) {
+    if (!window.FormData) {
+      alert('You are making all developpers in the world cringe. Upgrade your browser!');
+      return true;
+    }
+    var files = parsleyInstance.$element[0].files;
+    return files.length != 1  || files[0].size <= maxSize * 1024;
+  },
+  requirementType: 'integer',
+  messages: {
+    en: 'This file should not be larger than %s Kb',
+    fr: 'Ce fichier est plus grand que %s Kb.'
+  }
 });
